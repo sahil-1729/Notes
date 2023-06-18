973a4cf7d72064555c86ab9de6ee214bfd9bed87
sahil-1729 <sahil.maji@gmail.com>
2023-06-18 20:35:01 +0530

    format the project structure

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,17 @@
 //.env will come first
 require('dotenv').config()
 const express = require('express')
-const app = express()
-const cors = require('cors')
-const Note = require('./models/note')
-var morgan = require('morgan')
+
+// const app = express()
+const app = require('./app')
+
+// const cors = require('cors')
+// const Note = require('./models/note')
+
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+// var morgan = require('morgan')
 // const note = require('./models/note')
 
 // if(process.argv.length<3){
@@ -13,72 +20,70 @@ var morgan = require('morgan')
 // }
 // const password = process.argv[2]
 
-app.use(cors())
-app.use(express.static('build'))
-app.use(morgan('dev'))
+// app.use(cors())
+// app.use(express.static('build'))
+// app.use(morgan('dev'))
+
+//REQUEST LOGGER
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next()
+// }
+
 
 //this is json parser, this will take the data from the request and convert into object
-app.use(express.json())
+// app.use(express.json())
 
 
-// let notes = [  {
-//     id: 1,
-//     content: "HTML is easy",
-//     important: true
-// },
-// {    id: 2,
-//     content: "Browser can execute only JavaScript",    important: false
-// },
-// {    id: 3,
-//     content: "GET and POST are the most important methods of HTTP protocol",
-//     important: true  }
-// ]
 
-app.get('/api/notes',(request,response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-    // console.log(`mongo one`)
-  })
-})
+// app.get('/api/notes',(request,response) => {
+//   Note.find({}).then(notes => {
+//     response.json(notes)
+//     // console.log(`mongo one`)
+//   })
+// })
 
-app.post('/api/notes',(request,response,next) => {
-  const body = request.body
-  // console.log(`Here's the body ${body}`)
-  if(body.content === undefined){
-    return response.status(400).json({ error : 'content hi nai hai' })
-  }
-  const nNote = new Note({
-    content : body.content,
-    important : body.important || false
-  })
-  console.log(`Here's the new note${nNote}`)
-  nNote.save().then(savedNote => {
-    response.send(savedNote)
-  })
-    .catch(error => next(error))
-})
-app.get('/api/notes/:id',(request,response,next) => {
-  Note.findById(request.params.id).then(note => {
-    if(note){
-      response.json(note)
-    }else{
-      response.status(400).end()
-    }
-  })
-    .catch(error => next(error))
-})
+// app.post('/api/notes',(request,response,next) => {
+//   const body = request.body
+//   // console.log(`Here's the body ${body}`)
+//   if(body.content === undefined){
+//     return response.status(400).json({ error : 'content hi nai hai' })
+//   }
+//   const nNote = new Note({
+//     content : body.content,
+//     important : body.important || false
+//   })
+//   console.log(`Here's the new note${nNote}`)
+//   nNote.save().then(savedNote => {
+//     response.send(savedNote)
+//   })
+//     .catch(error => next(error))
+// })
+// app.get('/api/notes/:id',(request,response,next) => {
+//   Note.findById(request.params.id).then(note => {
+//     if(note){
+//       response.json(note)
+//     }else{
+//       response.status(400).end()
+//     }
+//   })
+//     .catch(error => next(error))
+// })
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-  else if(error.name === 'ValidationError'){
-    return response.status(400).json({ error: error.message })
-  }
+// const errorHandler = (error, request, response, next) => {
+//   console.error(error.message)
+//   if (error.name === 'CastError') {
+//     return response.status(400).send({ error: 'malformatted id' })
+//   }
+//   else if(error.name === 'ValidationError'){
+//     return response.status(400).json({ error: error.message })
+//   }
 
-  next(error)
-}
+//   next(error)
+// }
 
 
 // const generateId = () => {
@@ -112,35 +117,35 @@ const errorHandler = (error, request, response, next) => {
 // })
 
 //Here below are the routes, wherein in the first, there are two parameters, request contains the detailed information of the get method, the the response tells what to do after getting the request, in this case(in first route) its sending hello world
-app.get('/',(request,response) => {
-  response.send('<h1>Hello World</h1>')
-})
+// app.get('/',(request,response) => {
+//   response.send('<h1>Hello World</h1>')
+// })
 
-app.delete('/api/notes/:id',(request,response,next) => {
-  Note.findByIdAndDelete(request.params.id).then(result => {
-    response.status(204).end()
-  })
-    .catch(error => {
-      next(error)
-    })
-})
+// app.delete('/api/notes/:id',(request,response,next) => {
+//   Note.findByIdAndDelete(request.params.id).then(result => {
+//     response.status(204).end()
+//   })
+//     .catch(error => {
+//       next(error)
+//     })
+// })
 // app.delete('/api/notes/:id',(request,response)=>{
 //     const id = Number(request.params.id)
 //     notes = notes.filter(note => note.id !== id)
 //     response.status(204).end()
 // })
 
-app.put('/api/notes/:id',(request,response,next) => {
-  const { content,important } = request.body
-  // console.log(body)
-  // const nNote = {
-  //     content : body.content,
-  //     important : body.important
-  // }
-  Note.findByIdAndUpdate(request.params.id,{ content,important },{ new: true,runValidators: true,context: 'query' })
-    .then(result => response.json(result))
-    .catch(error => next(error))
-})
+// app.put('/api/notes/:id',(request,response,next) => {
+//   const { content,important } = request.body
+//   // console.log(body)
+//   // const nNote = {
+//   //     content : body.content,
+//   //     important : body.important
+//   // }
+//   Note.findByIdAndUpdate(request.params.id,{ content,important },{ new: true,runValidators: true,context: 'query' })
+//     .then(result => response.json(result))
+//     .catch(error => next(error))
+// })
 //the inverted commas makes the url unique, i.e its a combination of the resource type and a unique number
 // app.get('/api/notes/:id',(request,response)=>{
 //     const id = Number(request.params.id)
@@ -167,11 +172,12 @@ app.put('/api/notes/:id',(request,response,next) => {
 //     response.json(notes)
 // })
 
-app.use(errorHandler)
-//This will bind the server to port 3001
+// app.use(errorHandler)
+// //This will bind the server to port 3001
 const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT,() => {
+  logger.info(`Server running on port ${PORT}`)
+})
 
 
 
--- Backend/models/note.js
+++ Backend/models/note.js
@@ -2,22 +2,22 @@
 
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
+// mongoose.set('strictQuery', false)
 
 
-const url = process.env.MONGODB_URI
+// const url = process.env.MONGODB_URI
 
 
-console.log('connecting to', url)
+// console.log('connecting to', url)
 
-mongoose.connect(url)
+// mongoose.connect(url)
 
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+//   .then(result => {
+//     console.log('connected to MongoDB')
+//   })
+//   .catch((error) => {
+//     console.log('error connecting to MongoDB:', error.message)
+//   })
 
 const noteSchema = new mongoose.Schema({
   content: {
