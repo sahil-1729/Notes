9611beb565c692bd2b12afca686fdb3a47ab2841
sahil-1729 <sahil.maji@gmail.com>
2023-07-11 11:28:32 +0530

    feat: Add Cypress E2E test for login and front page functionality

--- Frontend/cypress/e2e/note_app.cy.js
+++ Frontend/cypress/e2e/note_app.cy.js
@@ -1,5 +1,14 @@
 describe('Note app',function(){
+  beforeEach(function(){
+    cy.visit('http://localhost:3000')  
+  });
+
   it('front page can be opened', function(){
-    cy.visit('http://localhost:3000/')
-  })
+    cy.contains('Notes')
+    cy.contains('Note app, Department of Computer Science, University of Helsinki 2023')
+  });
+
+  it('login form can be opened',()=>{
+    cy.contains('log in').click()
+  });
 })
