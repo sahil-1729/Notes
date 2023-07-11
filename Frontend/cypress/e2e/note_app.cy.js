c898aa232ab8b4f68f5a081eee76ad3a71e68d34
sahil-1729 <sahil.maji@gmail.com>
2023-07-11 20:46:27 +0530

    feat(login): add Cypress test for login form opening, user login, and saving new note

--- Frontend/cypress/e2e/note_app.cy.js
+++ Frontend/cypress/e2e/note_app.cy.js
@@ -11,4 +11,25 @@ describe('Note app',function(){
   it('login form can be opened',()=>{
     cy.contains('log in').click()
   });
+  it('user can login',function (){
+    cy.contains('log in').click()
+    cy.get('#username').type('username')
+    cy.get('#password').type('password')
+    cy.get('#login-button').click()
+    cy.contains('name logged in')
+  })
+  describe('user when logged in',function() {
+    beforeEach(function(){
+      cy.contains('log in').click()
+      cy.get('input:first').type('username')
+      cy.get('input:last').type('password')
+      cy.get('#login-button').click()
+    })
+    it('a new note can be created',function(){
+      cy.contains('new note').click()
+      cy.get('input').type('This is the note from testing')
+      cy.contains('save').click()
+      cy.contains('This is the note from testing')
+    })
+  })
 })
